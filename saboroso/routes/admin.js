d2bee5e08964cb3d61a92d3693711969eda9760d
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-10 15:03:37 -0300

    feat: home screen querying data in mysql;

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -31,7 +31,21 @@ router.get("/logout", function(req, res, next){
 
 router.get("/", function(req, res, next){
 
-        res.render("admin/index", admin.getParams(req));
+    admin.dashboard().then(data => {
+
+        res.render("admin/index", admin.getParams(req, {
+            data
+        }));
+
+    }).catch(err => {
+
+        console.error(err);
+
+    });
+
+        res.render("admin/index", admin.getParams(req, {
+
+        }));
 
 });
 
