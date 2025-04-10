59be659831def094af7747a2f81d655c982c960a
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-10 09:43:03 -0300

    feat: organizing content with Includes and refactoring headerand footer

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,23 +1,20 @@
 var conn = require('./../inc/db');
 var express = require('express');
+var menus = require('./../inc/menus');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-    `, (err, results)=>{
-      if (err) {
-        console.log(err);
-      }
 
-      res.render('index',
-         { title: 'Restaurante Saboroso!',
-          menus: results
-          });
+  menus.getMenus().then(results => {
 
-    });
+    res.render('index',
+      { title: 'Restaurante Saboroso!',
+       menus: results,
+       isHome: true
+       });
+
+  });
 
   });
 
@@ -33,10 +30,15 @@ router.get('/', function(req, res, next) {
 
   router.get('/menus', function(req, res, next ){
 
-    res.render('menus', {
-      title: 'Menus - Restaurante Saboroso!',
-      background: 'images/img_bg_1.jpg',
-      h1: 'Saboreie noss Menu!'
+    menus.getMenus().then(results =>{
+
+      res.render('menus', {
+        title: 'Menus - Restaurante Saboroso!',
+        background: 'images/img_bg_1.jpg',
+        h1: 'Saboreie nosso Menu!',
+        menus: results
+      });
+
     });
 
 });
