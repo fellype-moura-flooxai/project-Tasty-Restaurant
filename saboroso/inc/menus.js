984d8dfe490ccd9cfa60d65e41fee1315f4d239d
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-11 12:28:37 -0300

    feat: menu administration  changing data via post

--- saboroso/inc/menus.js
+++ saboroso/inc/menus.js
@@ -1,3 +1,4 @@
+const { param } = require('../routes');
 let conn = require('./db');
 let path = require('path');
 
@@ -25,22 +26,52 @@ module.exports = {
 
         fields.photo = `images/${path.parse(files.photo.path).base}`;
 
-        conn.query(`
-          INSERT INTO tb_menus (title, description, price, photo)
-          VALUES(?, ?, ?, ?)
-          `, [
-            fields.title,
-            fields.desciption,
-            fields.price,
-            `images/${files.photo.name}`
-          ], (err, results)=>{
+        let query, queryPhoto = '', params = [
+          fields.title,
+          fields.desciption,
+          fields.price
+        ];
+
+        if (files.photo.name) {
+
+          queryPhoto = ',photo = ?';
+
+          params.push(fields.photo);
+
+        }
+
+        if (parseInt(fields.id) > 0) {
+
+          params.push(fields.id);
+
+          query = `
+          UPDATE tb_menus
+          SET title = ?,
+              description = ?,
+              price = ?
+              ${queryPhoto}
+          WHERE id = ?
+          `;
+
+        } else {
+
+          if (!files.photo.name) {
+            reject('Envie a foto do prato.');
+          }
+
+          query = `
+            INSERT INTO tb_menus (title, description, price, photo)
+            VALUES(?, ?, ?, ?)
+          `;
+
+        }
+
+        conn.query(query, params, (err, results)=>{
 
             if (err) {
               reject(err);
             } else {
-
               resolve(results);
-
             }
 
           });
